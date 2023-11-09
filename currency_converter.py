40956abcf565fcd6f068c1734c02b26fdee355f0
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-09 15:00:30 +0100

    update objectives

---
+++ currency_converter.py
@@ -0,0 +1,4 @@
+# currency calculator to practice 'locale'
+# changing the language independend of cultural aspacts, like $12.34 and 12,34 €
+# aspects: ways to write balances, dates, etc
+# practising string formatting
